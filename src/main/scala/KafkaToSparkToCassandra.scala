385e09e478d28bc4aeebf37628a227fc5ff2e774
Shyam Nagarajan <shyamrock@gmail.com>
2019-09-10 07:39:15 -0500

    added the variable to parse

--- src/main/scala/KafkaToSparkToCassandra.scala
+++ src/main/scala/KafkaToSparkToCassandra.scala
@@ -17,17 +17,13 @@ object ToCassandraFromKafkaViaSpark extends SparkSessionFactory {
     // localhost kafka server
     val broker = "localhost:9092"
 
-    /*Here is an example massage which I get from a Kafka stream. It contains multiple jsons separated by \n
-    {"timestamp_ms": "1530305100936", "store_id": "Hermitage - 1", "sales_total" :'1255.89'}
+    /*
 
-    {"timestamp_ms": "1530305100936", "store_id": "Hermitage - 1", "sales_total" :'1255.89'}
-
-    {"item_id": "plates-234", "item_cost": 23.57, "item_name": "Plates", "timestamp_ms": "1530305100936", "store_id": "Hermitage - 1"}
-  {"item_id": "plates-235", "item_sold": 23, "item_name": "Plates", "timestamp_ms": "1530305100936", "store_id": "Hermitage - 1"}
+    {"item_id": "plates-235", "item_sold": 23, "item_name": "Plates", "timestamp_ms": "1530305100936", "store_id": "Hermitage - 1"}
 
     */
 
-    val dfraw = spark
+    val dframe = spark
     .readStream
     .format("kafka")
     .option("kafka.bootstrap.servers", broker)
@@ -35,15 +31,7 @@ object ToCassandraFromKafkaViaSpark extends SparkSessionFactory {
       .option("failOnDataLoss", false)
       .load()
 
-    dfraw.printSchema() // returns the schema of Kafka streaming
-    
-    /*val schema = StructType(
-      Seq(
-        StructField("store_id", StringType, false),
-        StructField("timestamp_ms", StringType, false),
-        StructField("sales_total", StringType, false)
-      )
-    )*/
+    dframe.printSchema() // returns the schema of Kafka streaming
 
 
     val schema = new StructType()
@@ -53,27 +41,19 @@ object ToCassandraFromKafkaViaSpark extends SparkSessionFactory {
       .add("timestamp_ms",StringType)
       .add("store_id",StringType)
      
-    
-    // Using a struct
-   // val schema = new StructType().add("a", new StructType().add("b", IntegerType))
 
-    val df = dfraw
+    val itemDF = dframe
     .selectExpr("CAST(value AS STRING)").as[String]
     .flatMap(_.split("\n"))
 
-    //val jsons = df.select(from_json($"value", schema) as "data").select("data.*")
-
-    val jsons = df.select(from_json(col("value").cast("string"), schema).as("data"))
+    val itemJson = itemDF.select(from_json(col("value").cast("string"), schema).as("data"))
       .select("data.*")
 
-    val parsed = jsons
+    val parsedItemJson = itemJson
       .withColumn("timestamp_dt", to_date(from_unixtime($"timestamp_ms"/1000.0, "yyyy-MM-dd HH:mm:ss.SSS")))
-
       .filter("item_id != ''")
 
-   // val df2 = df.withColumn("val_float", $"value".cast(FloatType))
-
-    val sink = parsed
+    val sink = parsedItemJson
     .writeStream
     .queryName("KafkaToCassandraForeach")
     .outputMode("update")
